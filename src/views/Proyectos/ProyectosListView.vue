f9fe0295f17ed9451f385f95eec0c9f327f3fefb
sjacomeUnergy <saraisabel@unergy.io>
2026-08-03 14:24:03 -0500

    fix(proyectos): simplificar tooltip de Inversores minigranja
    Quita el detalle tecnico (3x300+50+40 kW), queda 'Crea los 5 inversores tipicos para minigranjas'.

--- src/views/Proyectos/ProyectosListView.vue
+++ src/views/Proyectos/ProyectosListView.vue
@@ -5,7 +5,7 @@
       <template #actions>
         <Button label="Inversores minigranja" icon="pi pi-bolt" size="small" severity="secondary" outlined
                 :loading="invBackfillLoading" @click="previewInversoresBackfill"
-                v-tooltip.bottom="'Crea los 5 inversores típicos (3×300+50+40 kW)'" />
+                v-tooltip.bottom="'Crea los 5 inversores típicos para minigranjas'" />
         <Button label="Nuevo proyecto" icon="pi pi-plus" size="small" @click="openNew" />
       </template>
     </PageHeader>
